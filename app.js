b7a53aa21de5a857c94aba47826d79e63431b60f
vnphu <vonhuphu117@gmail.com>
2021-01-15 17:28:34 +0700

    fix

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const sizes = document.querySelector(".sizes");
 container.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  console.log(xAxis, yAxis);
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
